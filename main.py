6504b97ac224723203b58a7aec7c42cb28ac76d9
alind12 <44715071+alind12@users.noreply.github.com>
2019-02-20 09:41:39 -0600

    Add files via upload
    added re and further shortened code. added comments to explain what my code was doing.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, redirect, render_template
 import cgi
-
+import re  #using re library to verify email takes what I had in a longer loop and makes it simpler. Could also use this for password check and username.
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-@app.route("/")
+@app.route("/")#html form used to create page.
 def index():
     return render_template('index.html')
 
@@ -13,49 +13,39 @@ def index():
 def submitted(): #User submitted information.
     username = request.form['username']
     password = request.form['password']
-    verify = request.form['verify']
+    passcheck = request.form['passcheck']
     email = request.form['email']
-    #Index Error
-    username_error = ''
+    username_error = ''    #Index Error
     password_error = ''
-    verify_error = ''
+    passcheck_error = ''
     email_error = ''
-   #Username check
-    if username =="":
-        username_error = 'Must have a username!'
-        username = ''
-    elif len(username) > 20 or len(username) < 3:
-        username_error = 'Username length must be between 3-20 characters.'
-        username = ''
-    #Password check and verify loop
-    if password == '':
+    for un in username:   #Username check and verify loop, looks that there is not a empty field, space, and character count of 3-20 if dont meet the requirement error is shown.
+            if un.isspace():
+                username_error = 'Username cannot contain spaces.'
+            elif (len(username) < 3) or (len(username) > 20):
+                    username_error = 'Username needs to be 3-20 characters.'
+    if not username:
+            username_error = 'Not a valid username'
+            username = ''
+    if password == '':          #Password check and verify loop
         password_error = 'Must have a password'
         password = ''
-        if len(password) > 20 or len(password) < 3:
+    elif len(password) < 3 or len(password) > 20:
             password_error = 'Password length must be between 3-20 characters.'
             password = ''
     else:
-        if password != verify:
-            verify_error = "Passwords don't match."
-            verify = ''
-    #Email Check and verify loop.
-    if email:
-        if ("@") not in email:
-            email_error = 'email must have @!'
-        elif (".") not in email:
-            email_error = 'must have a period!'
-        elif " " in email:
-            email_error = "Emails do not have spaces!"
-        elif len(email) <= 3 or len(email) > 20:
-            email_error = "email to short"
-    if not password_error and not verify_error and not username_error and not email_error:
-    #Redirects to welcome page or shows what your error is. 
-        return redirect("/welcome?name={0}".format(username))
-    else:
-        return render_template('index.html', username_error=username_error,
-            password_error=password_error, verify_error=verify_error, email_error = email_error, 
-            username=username,
-            password=password, email=email)
+        if password != passcheck:
+            passcheck_error = "Passwords don't match."
+            passcheck = ''
+    if (email != '') and (not re.match('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)):        #Email Check and verify loop.
+            email_error = 'This is not a valid email, please check for space "@""." and length of 3-20 characters.'
+            email = ''   #Redirects to welcome page or shows what your error is. 
+    if (not username_error) and (not password_error) and (not passcheck_error) and (not email_error):
+            return redirect('/welcome?name={0}'.format(username))
+
+    return render_template('index.html', username=username, email=email,
+                           username_error=username_error, password_error=password_error,
+                           passcheck_error=passcheck_error, email_error=email_error)
 
 @app.route('/welcome')
 def welcome():
